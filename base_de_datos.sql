0749bcd0010aa33733187b449b30ae6db4aa8d78
Edwin Geovani Sosa Sosa <5062090@incb.edu.sv>
2026-05-26 07:33:29 -0600

    Documentadas consultas avanzadas y reportes con INNER JOIN

--- base_de_datos.sql
+++ base_de_datos.sql
@@ -35,3 +35,21 @@ enteros)
 INSERT INTO productos (nombre_producto, categoria_id, stock, precio) VALUES
 ('Laptop Dell Inspiron 15', 1, 15, 720.00),
 ('Mouse Inalámbrico Logitech', 2, 25, 12.00);
+
+
+/*
+REPORTES RELACIONALES AVANZADOS (GUÍA 11)
+
+1. Inventario general
+*/
+SELECT p.id, p.nombre_producto, c.nombre_categoria, p.stock, p.precio
+FROM productos p
+INNER JOIN categorias c ON p.categoria_id = c.id;
+
+/*
+2. Inventario filtrado por Accesorios
+*/
+SELECT p.id, p.nombre_producto, c.nombre_categoria, p.stock, p.precio
+FROM productos p
+INNER JOIN categorias c ON p.categoria_id = c.id
+WHERE c.nombre_categoria = 'Accesorios';
